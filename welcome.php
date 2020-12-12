12386bf9f41896ab5089585bea0d1beddaa17e41
hjortenmorten14 <Jonathanrunge99@hotmail.com>
2020-12-12 01:43:05 +0100

    temporarily added loginbox

--- welcome.php
+++ welcome.php
@@ -1,18 +1,18 @@
 <?php
 include "database.php";
-$username= $_POST["username"];
-$password= $_POST["password"];
+$username = $_POST["username"];
+$password = $_POST["password"];
 
 
-$sql = "SELECT * FROM users WHERE username ='".$username."' AND password ='".$password."' ";
+$sql = "SELECT * FROM users WHERE username ='" . $username . "' AND password ='" . $password . "' ";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo $row["id"]."<br>";
-    echo $row["username"]."<br>";
-    echo "hello ".$row["nickname"];
+  while ($row = $result->fetch_assoc()) {
+    echo $row["id"] . "<br>";
+    echo $row["username"] . "<br>";
+    echo "hello " . $row["nickname"];
   }
 } else {
   echo "login failed";
@@ -21,7 +21,16 @@ $conn->close();
 
 ?>
 <html>
-<body>
 
+<body>
+  <div class="login-box">
+    <h1>Log in</h1>
+    <form class="username" action="./welcome.php" method="post">
+      Name: <input type="text" name="username"><br>
+      password: <input type="password" name="password"><br>
+      <input type="submit" value="Log in" class="login-button">
+    </form>
+  </div>
 </body>
+
 </html>
