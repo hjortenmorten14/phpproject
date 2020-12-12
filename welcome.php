197a7544ef0c7bc5706a66ffd1ae462f23668b4a
hjortenmorten14 <Jonathanrunge99@hotmail.com>
2020-12-12 02:26:14 +0100

    refactor welcome.php to work as login handler

--- welcome.php
+++ welcome.php
@@ -9,28 +9,11 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
-  while ($row = $result->fetch_assoc()) {
-    echo $row["id"] . "<br>";
-    echo $row["username"] . "<br>";
-    echo "hello " . $row["nickname"];
-  }
+
+  session_start();
+  $_SESSION["loggedin"] = true;
+  header("Location: index.php");
 } else {
   echo "login failed";
 }
 $conn->close();
-
-?>
-<html>
-
-<body>
-  <div class="login-box">
-    <h1>Log in</h1>
-    <form class="username" action="./welcome.php" method="post">
-      Name: <input type="text" name="username"><br>
-      password: <input type="password" name="password"><br>
-      <input type="submit" value="Log in" class="login-button">
-    </form>
-  </div>
-</body>
-
-</html>
